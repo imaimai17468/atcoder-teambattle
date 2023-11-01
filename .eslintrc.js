9ae3396828c36dbb96b8ba983da62f42be851638
imaimai17468 <tosiki_801_life@outlook.jp>
2023-11-01 16:06:56 +0900

    feat: nextのruleをeslintに追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:tailwindcss/recommended",
     "plugin:jsx-a11y/recommended",
+    "plugin:@next/next/recommended",
   ],
   globals: {
     Atomics: "readonly",
