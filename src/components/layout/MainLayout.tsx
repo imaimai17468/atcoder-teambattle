b35279fe57d8c0b3e32ceab4ed143349e7f94fd7
imaimai17468 <tosiki_801_life@outlook.jp>
2023-11-05 20:41:19 +0900

    fix: 文字色を濃いグレーに修正

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export const MainLayout: React.FC<Props> = ({ children }: Props) => {
   return (
-    <main className="flex min-h-screen flex-col gap-4">
+    <main className="flex min-h-screen flex-col gap-4 text-gray-900">
       <Header />
       {children}
     </main>
