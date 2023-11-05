a769812249bc2eea269ee5a3b3ece9bf66db9dce
imaimai17468 <tosiki_801_life@outlook.jp>
2023-11-06 00:39:05 +0900

    feat: dashboardのレスポンシブ対応

--- src/components/screen/DashBoard/DashBoard.tsx
+++ src/components/screen/DashBoard/DashBoard.tsx
@@ -1,6 +1,9 @@
 import { Battle } from "@/schema/Battle.type";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
+import { CaretSortIcon } from "@radix-ui/react-icons";
+import { Button } from "@/components/ui/button";
+import clsx from "clsx";
 
 type DashBoardProps = {
   battles: Battle[];
@@ -26,27 +29,39 @@ export const DashBoard: React.FC<DashBoardProps> = ({
     }, 0);
   }, [battles]);
 
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="flex flex-col gap-4 rounded-lg border bg-gray-50 p-4 shadow-md">
-      <h1 className="text-2xl font-semibold">DashBoard</h1>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-semibold">DashBoard</h1>
+        <Button variant="outline" onClick={() => setIsOpen(!isOpen)}>
+          <CaretSortIcon />
+        </Button>
+      </div>
+      <div
+        className={clsx(
+          "grid grid-cols-1 gap-4 sm:grid-cols-3",
+          isOpen ? "grid sm:hidden" : "hidden sm:grid",
+        )}
+      >
         <Card>
           <CardHeader className="text-sm">Running Battles</CardHeader>
-          <CardContent className="flex gap-2 text-xl font-bold">
+          <CardContent className="flex justify-center gap-2 text-xl font-bold sm:justify-start">
             <p className="text-emerald-400">{battles.length}</p>
             <p> Battles</p>
           </CardContent>
         </Card>
         <Card>
           <CardHeader className="text-sm">Active Teams</CardHeader>
-          <CardContent className="flex gap-2 text-xl font-bold">
+          <CardContent className="flex justify-center gap-2 text-xl font-bold sm:justify-start">
             <p className="text-emerald-400"> {activeTeams}</p>
             <p>Teams</p>
           </CardContent>
         </Card>
         <Card>
           <CardHeader className="text-sm">Active Users</CardHeader>
-          <CardContent className="flex gap-2 text-xl font-bold">
+          <CardContent className="flex justify-center gap-2 text-xl font-bold sm:justify-start">
             <p className="text-emerald-400"> {activeUsers}</p>
             <p>Users</p>
           </CardContent>
