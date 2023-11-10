d60737869f6be6b7fe1adacda6e4f7c79fc95de4
imaimai17468 <tosiki_801_life@outlook.jp>
2023-11-10 13:19:36 +0900

    feat: Add battle list tables and dashboard to BattlePage, and update Home page UI

--- src/pages/battle/index.tsx
+++ src/pages/battle/index.tsx
@@ -1,7 +1,58 @@
+import BattleListTable from "@/components/screen/BattleListTable/BattleListTable";
+import { useState, useEffect } from "react";
+import { Battle } from "@/schema/Battle.type";
+import { createMockAllTimeBattle } from "@/repositories/createMockBattle";
+import { DashBoard } from "@/components/screen/DashBoard";
+import { DoubleArrowDownIcon } from "@radix-ui/react-icons";
 import { NextPage } from "next";
 
 export const BattlePage: NextPage = () => {
-  return <div></div>;
+  const [AllBattles, setAllBattles] = useState<{
+    upcoming: Battle[];
+    running: Battle[];
+    recent: Battle[];
+  }>();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const { upcomingBattles, runningBattles, recentBattles } =
+      createMockAllTimeBattle();
+    setAllBattles({
+      upcoming: upcomingBattles,
+      running: runningBattles,
+      recent: recentBattles,
+    });
+    setIsLoading(false);
+  }, []);
+
+  return (
+    <div className="flex flex-col gap-8">
+      <DashBoard battles={AllBattles?.running || null} isLoading={isLoading} />
+      <div className="flex flex-col gap-4">
+        <BattleListTable
+          battles={AllBattles?.running || null}
+          variant="running"
+          isLoading={isLoading}
+        />
+        <div className="my-8 flex w-full justify-center">
+          <DoubleArrowDownIcon className="-translate-y-1/2 animate-bounce" />
+        </div>
+        <BattleListTable
+          battles={AllBattles?.upcoming || null}
+          variant="upcoming"
+          isLoading={isLoading}
+        />
+        <div className="my-8 flex w-full justify-center">
+          <DoubleArrowDownIcon className="-translate-y-1/2 animate-bounce" />
+        </div>
+        <BattleListTable
+          battles={AllBattles?.recent || null}
+          variant="recent"
+          isLoading={isLoading}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default BattlePage;
