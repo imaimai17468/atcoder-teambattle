e2ec0cb1534d4f972c5cebe5d7a9e2498f9dfe08
imaimai17468 <tosiki_801_life@outlook.jp>
2023-11-16 02:42:50 +0900

    feat: Add BattleAnalytics component with ScoreChart and useAnalyticsValue hook

---
+++ src/components/screen/BattleAnalytics/BattleAnalytics.tsx
@@ -0,0 +1,24 @@
+import { Battle } from "@/schema/Battle.type";
+import { ScoreChart } from "./components/ScoreChart";
+import { useAnalyticsValue } from "@/hooks/useAnalyticsValue";
+
+type BattleAnalyticsProps = {
+  battle: Battle;
+};
+
+export const BattleAnalytics: React.FC<BattleAnalyticsProps> = ({
+  battle,
+}: BattleAnalyticsProps) => {
+  const { chartData, TimeLineDataList } = useAnalyticsValue(battle);
+  console.log(TimeLineDataList);
+  return (
+    <div className="flex gap-8">
+      <div className="w-2/3">
+        <ScoreChart chartData={chartData} />
+      </div>
+      <div>TimeLine</div>
+    </div>
+  );
+};
+
+export default BattleAnalytics;
---
+++ src/components/screen/BattleAnalytics/index.ts
@@ -0,0 +1,1 @@
+export { default as BattleAnalytics } from "./BattleAnalytics";
