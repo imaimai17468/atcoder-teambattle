20f965b3666fe28b106c550270c72b35cd848c78
imaimai17468 <tosiki_801_life@outlook.jp>
2023-11-18 04:08:16 +0900

    fix: Add user profile link and update user avatar UI.

--- src/components/common/UserAvatar/UserAvatar.tsx
+++ src/components/common/UserAvatar/UserAvatar.tsx
@@ -5,12 +5,18 @@ import {
   HoverCardTrigger,
 } from "@/components/ui/hovercard";
 import { User } from "@/schema/User.type";
+import { useRouter } from "next/router";
+import { CLIENT_PATH } from "@/constants/clientpath";
+import { Button } from "@/components/ui/button";
+import { CardDescription } from "@/components/ui/card";
 
 type UserAvatarProps = {
   user: User;
 };
 
 export const UserAvatar = ({ user }: UserAvatarProps) => {
+  const router = useRouter();
+
   return (
     <HoverCard key={user.name}>
       <HoverCardTrigger>
@@ -19,16 +25,40 @@ export const UserAvatar = ({ user }: UserAvatarProps) => {
           <AvatarFallback>{user.name}</AvatarFallback>
         </Avatar>
       </HoverCardTrigger>
-      <HoverCardContent>
-        <div className="flex justify-between gap-4">
-          <Avatar>
-            <AvatarImage src={user.icon} alt={user.name} />
-            <AvatarFallback>{user.name}</AvatarFallback>
-          </Avatar>
-          <div className="flex flex-col justify-start gap-2">
-            <h4 className="text-sm font-semibold">{user.name}</h4>
-            <p className="text-sm">{user.bio}</p>
+      <HoverCardContent
+        className="w-80"
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <div className="flex flex-col gap-4">
+          <div className="flex items-center gap-4">
+            <Avatar>
+              <AvatarImage src={user.icon} alt={user.name} />
+              <AvatarFallback>{user.name}</AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col">
+              <CardDescription>{user.AtCoderID}</CardDescription>
+              <p className="text-lg font-semibold">{user.name}</p>
+            </div>
           </div>
+          <CardDescription>
+            {user.occupation && <p>{user.occupation}</p>}
+            {user.organization && <p>at {user.organization}</p>}
+          </CardDescription>
+          {user.bio && <p className="text-sm">{user.bio}</p>}
+          <Button
+            className="ml-auto w-fit"
+            size="sm"
+            onClick={() => {
+              router.push({
+                pathname: CLIENT_PATH.USER,
+                query: { userId: user.id },
+              });
+            }}
+          >
+            view Profile
+          </Button>
         </div>
       </HoverCardContent>
     </HoverCard>
