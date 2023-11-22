d80681eaa4304e0042aec7d52642e6cf6ebcae00
imaimai17468 <tosiki_801_life@outlook.jp>
2023-11-22 23:46:32 +0900

    fix: Remove sleep function call in UserProfileContent

--- src/components/screen/UserProfileContent/UserProfileContent.tsx
+++ src/components/screen/UserProfileContent/UserProfileContent.tsx
@@ -1,10 +1,9 @@
-import { sleep } from "@/utils/sleep";
 import { createMockUser } from "../../../repositories/createMockUser";
 import { UserProfileCard } from "../UserProfileCard/UserProfileCard";
 
 export const UserProfileContent = async () => {
   const user = await createMockUser();
-  await sleep(1000);
+
   return <UserProfileCard user={user} />;
 };
 
