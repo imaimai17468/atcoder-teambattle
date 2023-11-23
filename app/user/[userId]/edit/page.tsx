154e74a2ced22916d9a6010a732d0c4b1d0de25d
imaimai17468 <tosiki_801_life@outlook.jp>
2023-11-24 03:50:09 +0900

    feat: Add user edit page and update client path

---
+++ app/user/[userId]/edit/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+import { UserProfileContent } from "@/components/screen/UserProfileContent";
+import { Suspense } from "react";
+import { LoadingUserProfileContent } from "@/components/screen/LoadingUserProfileContent";
+
+export const metadata: Metadata = {
+  title: "User",
+};
+
+export default async function UserPage() {
+  return (
+    <Suspense fallback={<LoadingUserProfileContent isEdit />}>
+      <UserProfileContent isEdit />
+    </Suspense>
+  );
+}
