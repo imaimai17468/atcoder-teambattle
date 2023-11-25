d48ff83eecd6e41dd3e3f90a3b00c99f51b9a14a
imaimai17468 <tosiki_801_life@outlook.jp>
2023-11-25 18:32:19 +0900

    fix: Update UserSchema validation rules

--- src/schema/User.type.ts
+++ src/schema/User.type.ts
@@ -1,19 +1,50 @@
 import { z } from "zod";
 
+export const ATCODER_ID_MAX_LENGTH = 16;
+export const NAME_MAX_LENGTH = 32;
+export const BIO_MAX_LENGTH = 1000;
+export const OCCUPATION_MAX_LENGTH = 32;
+export const ORGANIZATION_MAX_LENGTH = 32;
+
 export const UserLinksSchema = z.object({
-  github: z.string().nullable(),
-  twitter: z.string().nullable(),
-  website: z.string().nullable(),
+  github: z.string().url({ message: "Invalid URL" }).nullable(),
+  twitter: z.string().url({ message: "Invalid URL" }).nullable(),
+  website: z.string().url({ message: "Invalid URL" }).nullable(),
 });
 
 export const UserSchema = z.object({
   id: z.string(),
-  AtCoderID: z.string(),
-  name: z.string(),
+  atcoderId: z
+    .string()
+    .min(1, { message: "AtCoder ID is required" })
+    .max(ATCODER_ID_MAX_LENGTH, {
+      message: `AtCoder ID must be less than ${ATCODER_ID_MAX_LENGTH} characters`,
+    }),
+  name: z
+    .string()
+    .min(1, { message: "Name is required" })
+    .max(NAME_MAX_LENGTH, {
+      message: `Name must be less than ${NAME_MAX_LENGTH} characters`,
+    }),
   icon: z.string(),
-  bio: z.string().nullable(),
-  occupation: z.string().nullable(),
-  organization: z.string().nullable(),
+  bio: z
+    .string()
+    .max(BIO_MAX_LENGTH, {
+      message: `Bio must be less than ${BIO_MAX_LENGTH} characters`,
+    })
+    .nullable(),
+  occupation: z
+    .string()
+    .max(OCCUPATION_MAX_LENGTH, {
+      message: `Occupation must be less than ${OCCUPATION_MAX_LENGTH} characters`,
+    })
+    .nullable(),
+  organization: z
+    .string()
+    .max(ORGANIZATION_MAX_LENGTH, {
+      message: `Organization must be less than ${ORGANIZATION_MAX_LENGTH} characters`,
+    })
+    .nullable(),
   links: UserLinksSchema,
 });
 
