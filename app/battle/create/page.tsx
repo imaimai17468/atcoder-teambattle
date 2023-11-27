6a1b8a5ff96012bef27de5873295cda6f67a3f51
imaimai17468 <tosiki_801_life@outlook.jp>
2023-11-27 22:32:34 +0900

    feat: Add CreateBattleForm component with Suspense fallback

--- app/battle/create/page.tsx
+++ app/battle/create/page.tsx
@@ -1,9 +1,16 @@
+import { LoadingAlert } from "@/components/common/LoadingAlert";
+import { CreateBattleForm } from "@/components/screen/CreateBattleForm";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Create Battle",
 };
 
 export default function CreateBattlesPage() {
-  return <div>create battle</div>;
+  return (
+    <Suspense fallback={<LoadingAlert />}>
+      <CreateBattleForm />
+    </Suspense>
+  );
 }
