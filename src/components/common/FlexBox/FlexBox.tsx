f1c58381f6aa4671f1967eb144b7b80b8109583c
imaimai17468 <tosiki_801_life@outlook.jp>
2023-12-02 21:52:20 +0900

    feat: Add FlexBox component

---
+++ src/components/common/FlexBox/FlexBox.tsx
@@ -0,0 +1,44 @@
+import clsx from "clsx";
+import { forwardRef } from "react";
+import styles from "./FlexBox.module.scss";
+import { HTMLAttributes, ReactNode } from "react";
+
+type divProps = NonNullable<JSX.IntrinsicElements["div"]["style"]>;
+
+export type FlexBoxProps = {
+  children?: ReactNode;
+  margin?: divProps["margin"];
+  padding?: divProps["padding"];
+  flexDirection?: divProps["flexDirection"];
+  flexWrap?: divProps["flexWrap"];
+  justifyContent?: divProps["justifyContent"];
+  alignItems?: divProps["alignItems"];
+  alignContent?: divProps["alignContent"];
+  flexBasis?: divProps["flexBasis"];
+  flexGrow?: divProps["flexGrow"];
+  flexShrink?: divProps["flexShrink"];
+  width?: divProps["width"];
+  height?: divProps["height"];
+  overflow?: divProps["overflow"];
+  style?: JSX.IntrinsicElements["div"]["style"];
+  gap?: divProps["gap"];
+  className?: HTMLAttributes<HTMLLIElement>["className"];
+};
+
+const FlexBox = forwardRef<HTMLDivElement, FlexBoxProps>((props, ref) => {
+  const { children, style, className, ...stylePlops } = props;
+  return (
+    <div
+      ref={ref}
+      className={clsx(styles.flex, className)}
+      style={{ ...stylePlops, ...style }}
+    >
+      {children}
+    </div>
+  );
+});
+
+const [displayName] = Object.keys({ FlexBox });
+FlexBox.displayName = displayName;
+
+export default FlexBox;
