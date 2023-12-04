973bb80eeddb8d4fc7fe1e07f4222b92c9673639
imaimai17468 <tosiki_801_life@outlook.jp>
2023-12-04 21:41:34 +0900

    feat: Add difficulty field to createMockProblem function

--- src/schema/Problem.type.ts
+++ src/schema/Problem.type.ts
@@ -4,6 +4,7 @@ export const ProblemSchema = z.object({
   name: z.string(),
   score: z.number(),
   link: z.string(),
+  difficulty: z.number(),
 });
 
 export type Problem = z.infer<typeof ProblemSchema>;
