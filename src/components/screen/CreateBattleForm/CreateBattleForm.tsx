2f2f1fdf6b6970b23c18fa4934b5e5b637de65ac
imaimai17468 <tosiki_801_life@outlook.jp>
2023-12-06 20:07:52 +0900

    feat: Add useCreateBattleForm hook to CreateBattleForm

--- src/components/screen/CreateBattleForm/CreateBattleForm.tsx
+++ src/components/screen/CreateBattleForm/CreateBattleForm.tsx
@@ -14,56 +14,73 @@ import { ProblemSetContent } from "./components/ProblemSetContent";
 import { createMockProblems } from "@/repositories/createMockProblem";
 import { ExpectedTeamContent } from "./components/ExpectedTeamContent";
 import { createMockUsers } from "@/repositories/createMockUser";
+import { useCreateBattleForm } from "./hooks/useCreateBattleForm";
 
 export const CreateBattleForm: React.FC = async () => {
   const problems = await createMockProblems(30);
   const users = await createMockUsers(30);
 
+  const { onSubmit, register, isDirty, isSubmitting, isValid } =
+    useCreateBattleForm();
+
   return (
-    <Card className="mb-48">
-      <CardHeader>
-        <CardTitle>Create Battle</CardTitle>
-        <Separator />
-      </CardHeader>
-      <CardContent className="flex flex-col gap-8">
-        <div className="flex flex-col gap-4">
-          <CardTitle>Basic Info</CardTitle>
-          <div className="flex flex-col gap-2">
-            <p>Title</p>
-            <Input className="w-72" />
-          </div>
-          <div className="flex flex-col gap-2">
-            <p>Description</p>
-            <Textarea />
-          </div>
-          <div className="flex flex-col gap-2">
-            <p>Start Time</p>
-            <div className="flex gap-2">
-              <DatePicker />
-              <Input type="time" className="w-32" />
+    <form onSubmit={onSubmit}>
+      <Card className="mb-48">
+        <CardHeader>
+          <CardTitle>Create Battle</CardTitle>
+          <Separator />
+        </CardHeader>
+        <CardContent className="flex flex-col gap-8">
+          <div className="flex flex-col gap-4">
+            <CardTitle>Basic Info</CardTitle>
+            <div className="flex flex-col gap-2">
+              <p>Title</p>
+              <Input
+                className="w-72"
+                required
+                type="text"
+                placeholder="Battle Title"
+                {...register("title")}
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <p>Description</p>
+              <Textarea
+                placeholder="Battle Description"
+                {...register("description")}
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <p>Start Time</p>
+              <div className="flex gap-2">
+                <DatePicker />
+                <Input type="time" className="w-32" />
+              </div>
+            </div>
+            <div className="flex flex-col gap-2">
+              <p>End Time</p>
+              <div className="flex gap-2">
+                <DatePicker />
+                <Input type="time" className="w-32" />
+              </div>
             </div>
           </div>
-          <div className="flex flex-col gap-2">
-            <p>End Time</p>
-            <div className="flex gap-2">
-              <DatePicker />
-              <Input type="time" className="w-32" />
-            </div>
+          <div className="flex flex-col gap-4">
+            <CardTitle>Expected Teams</CardTitle>
+            <ExpectedTeamContent users={users} />
           </div>
-        </div>
-        <div className="flex flex-col gap-4">
-          <CardTitle>Expected Teams</CardTitle>
-          <ExpectedTeamContent users={users} />
-        </div>
-        <div className="flex flex-col gap-4">
-          <CardTitle>Battle ProblemSet</CardTitle>
-          <ProblemSetContent problems={problems} />
-        </div>
-      </CardContent>
-      <CardFooter className="flex justify-end">
-        <Button>Create</Button>
-      </CardFooter>
-    </Card>
+          <div className="flex flex-col gap-4">
+            <CardTitle>Battle ProblemSet</CardTitle>
+            <ProblemSetContent problems={problems} />
+          </div>
+        </CardContent>
+        <CardFooter className="flex justify-end">
+          <Button type="submit" disabled={!isDirty || isSubmitting || !isValid}>
+            Create
+          </Button>
+        </CardFooter>
+      </Card>
+    </form>
   );
 };
 
