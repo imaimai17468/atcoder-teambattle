f9ff3bdfd4379d1e0997cb670cf1ea1594fe4f95
imaimai17468 <tosiki_801_life@outlook.jp>
2023-12-10 23:51:47 +0900

    feat: Update BattleSchema in Battle.type.ts

--- src/schema/Battle.type.ts
+++ src/schema/Battle.type.ts
@@ -2,14 +2,30 @@ import { z } from "zod";
 import { ScoreSchema } from "./Score.type";
 import { ProblemSchema } from "./Problem.type";
 import { UserSchema } from "./User.type";
+import { charMinLimitError, charMaxLimitError } from "@/utils/createErrors";
+import { TeamSchema } from "./Team.type";
+
+const TITLE_MAX_LENGTH = 50;
+const DESCRIPTION_MAX_LENGTH = 1000;
 
 export const BattleSchema = z.object({
   id: z.string(),
-  title: z.string(),
-  description: z.string(),
+  title: z
+    .string()
+    .min(1, { message: charMinLimitError("Title", 1) })
+    .max(TITLE_MAX_LENGTH, {
+      message: charMaxLimitError("Title", TITLE_MAX_LENGTH),
+    }),
+  description: z
+    .string()
+    .min(1, { message: charMinLimitError("Description", 1) })
+    .max(DESCRIPTION_MAX_LENGTH, {
+      message: charMaxLimitError("Description", DESCRIPTION_MAX_LENGTH),
+    }),
   startDate: z.number(),
   endDate: z.number(),
-  scores: z.array(ScoreSchema),
+  teams: z.array(TeamSchema).nullable(),
+  scores: z.array(ScoreSchema).nullable(),
   problems: z.array(ProblemSchema),
   owner: UserSchema,
   createdAt: z.number(),
