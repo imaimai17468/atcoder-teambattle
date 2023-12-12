f36a73d42e2d896b19451b8996bb9cc8de5175ca
imaimai17468 <tosiki_801_life@outlook.jp>
2023-12-12 22:35:48 +0900

    fix: Fix validation errors in ScoreSchema and TeamSchema

--- src/schema/Team.type.ts
+++ src/schema/Team.type.ts
@@ -1,9 +1,21 @@
 import { z } from "zod";
 import { UserSchema } from "./User.type";
+import {
+  charMinLimitError,
+  charMaxLimitError,
+  arrayMinLimitError,
+} from "@/utils/createErrors";
 
 export const TeamSchema = z.object({
-  name: z.string(),
-  members: z.array(UserSchema),
+  name: z
+    .string()
+    .min(1, { message: charMinLimitError("Name", 1) })
+    .max(50, {
+      message: charMaxLimitError("Name", 50),
+    }),
+  members: z.array(UserSchema).min(1, {
+    message: arrayMinLimitError("Members", 1),
+  }),
 });
 
 export type Team = z.infer<typeof TeamSchema>;
