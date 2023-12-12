d020de258deba55f883bd29d843741075d56bfc7
imaimai17468 <tosiki_801_life@outlook.jp>
2023-12-12 11:57:22 +0900

    feat: Refactor BattleSchema and add validation messages

--- src/schema/Battle.type.ts
+++ src/schema/Battle.type.ts
@@ -2,35 +2,49 @@ import { z } from "zod";
 import { ScoreSchema } from "./Score.type";
 import { ProblemSchema } from "./Problem.type";
 import { UserSchema } from "./User.type";
-import { charMinLimitError, charMaxLimitError } from "@/utils/createErrors";
+import {
+  charMinLimitError,
+  charMaxLimitError,
+  arrayMinLimitError,
+  dateMinLimitError,
+} from "@/utils/createErrors";
 import { INITIAL_USER } from "./User.type";
 
 const TITLE_MAX_LENGTH = 50;
 const DESCRIPTION_MAX_LENGTH = 1000;
 
-export const BattleSchema = z.object({
-  id: z.string(),
-  title: z
-    .string()
-    .min(1, { message: charMinLimitError("Title", 1) })
-    .max(TITLE_MAX_LENGTH, {
-      message: charMaxLimitError("Title", TITLE_MAX_LENGTH),
+export const BattleSchema = z
+  .object({
+    id: z.string(),
+    title: z
+      .string()
+      .min(1, { message: charMinLimitError("Title", 1) })
+      .max(TITLE_MAX_LENGTH, {
+        message: charMaxLimitError("Title", TITLE_MAX_LENGTH),
+      }),
+    description: z
+      .string()
+      .min(1, { message: charMinLimitError("Description", 1) })
+      .max(DESCRIPTION_MAX_LENGTH, {
+        message: charMaxLimitError("Description", DESCRIPTION_MAX_LENGTH),
+      }),
+    startDate: z.number(),
+    endDate: z.number().min(Date.now(), {
+      message: dateMinLimitError("End date", new Date(Date.now())),
     }),
-  description: z
-    .string()
-    .min(1, { message: charMinLimitError("Description", 1) })
-    .max(DESCRIPTION_MAX_LENGTH, {
-      message: charMaxLimitError("Description", DESCRIPTION_MAX_LENGTH),
+    scores: z.array(ScoreSchema).nullable(),
+    problems: z.array(ProblemSchema).min(1, {
+      message: arrayMinLimitError("Problems", 1),
     }),
-  startDate: z.number(),
-  endDate: z.number(),
-  scores: z.array(ScoreSchema).nullable(),
-  problems: z.array(ProblemSchema),
-  owner: UserSchema,
-  createdAt: z.number(),
-  updatedAt: z.number().nullable(),
-  deletedAt: z.number().nullable(),
-});
+    owner: UserSchema,
+    createdAt: z.number(),
+    updatedAt: z.number().nullable(),
+    deletedAt: z.number().nullable(),
+  })
+  .refine((data) => data.startDate < data.endDate, {
+    message: "Start date must be before end date.",
+    path: ["endDate"],
+  });
 
 export type Battle = z.infer<typeof BattleSchema>;
 
@@ -40,7 +54,7 @@ export const INITIAL_BATTLE: Battle = {
   description: "",
   startDate: 0,
   endDate: 0,
-  scores: [],
+  scores: null,
   problems: [],
   owner: INITIAL_USER,
   createdAt: 0,
