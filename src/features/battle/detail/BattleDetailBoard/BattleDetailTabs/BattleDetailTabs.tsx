01a9b4628a91a9025584ee97bc27434e363fa326
imaimai17468 <tosiki_801_life@outlook.jp>
2023-12-16 15:00:58 +0900

    fix: Update BattleDetailTabs component

--- src/features/battle/detail/BattleDetailBoard/BattleDetailTabs/BattleDetailTabs.tsx
+++ src/features/battle/detail/BattleDetailBoard/BattleDetailTabs/BattleDetailTabs.tsx
@@ -13,17 +13,17 @@ export const BattleDetailTabs: React.FC<BattleDetailTabsProps> = ({
   battle,
 }: BattleDetailTabsProps) => {
   return (
-    <Tabs defaultValue="problems">
+    <Tabs defaultValue="standings">
       <TabsList className="mb-4 border">
-        <TabsTrigger value="problems">Problems</TabsTrigger>
         <TabsTrigger value="standings">Standings</TabsTrigger>
+        <TabsTrigger value="problems">Problems</TabsTrigger>
         <TabsTrigger value="analytics">Analytics</TabsTrigger>
       </TabsList>
-      <TabsContent value="problems">
-        <ProblemListTable problems={battle.problems} />
-      </TabsContent>
       <TabsContent value="standings">
         <StandingsTable battle={battle} startDate={battle.startDate} />
+      </TabsContent>
+      <TabsContent value="problems">
+        <ProblemListTable problems={battle.problems} />
       </TabsContent>
       <TabsContent value="analytics">
         <BattleAnalytics battle={battle} />
