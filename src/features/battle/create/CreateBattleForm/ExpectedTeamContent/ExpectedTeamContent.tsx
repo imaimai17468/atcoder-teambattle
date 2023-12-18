14757f371c516dea303f5f1f2db6d03b7dd35478
imaimai17468 <tosiki_801_life@outlook.jp>
2023-12-18 18:18:28 +0900

    refactor: Add SearchInput component and update imports

--- src/features/battle/create/CreateBattleForm/ExpectedTeamContent/ExpectedTeamContent.tsx
+++ src/features/battle/create/CreateBattleForm/ExpectedTeamContent/ExpectedTeamContent.tsx
@@ -6,9 +6,9 @@ import { Control, useWatch } from "react-hook-form";
 import { VList } from "virtua";
 
 import { useExpectedTeamContent } from "./hooks/useExpectedTeamContent";
-import { SearchInput } from "../SearchInput/SearchInput";
 
 import { LoadingAlert } from "@/components/common/LoadingAlert";
+import { SearchInput } from "@/components/common/SearchInput";
 import { UserAvatar } from "@/components/common/UserAvatar";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
