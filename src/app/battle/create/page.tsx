5181b28987511a877318c706be8083111234c7c6
imaimai17468 <tosiki_801_life@outlook.jp>
2023-12-24 01:01:38 +0900

    Refactor CreateBattlesPage component to use Suspense and LoadingAlert

--- src/app/battle/create/page.tsx
+++ src/app/battle/create/page.tsx
@@ -1,11 +1,17 @@
 import { Metadata } from "next";
+import { Suspense } from "react";
 
-import { BattleForm } from "@/features/battle/create/BattleForm";
+import { LoadingAlert } from "@/components/common/LoadingAlert";
+import { CreateBattleContent } from "@/features/battle/create/CreateBattleContent";
 
 export const metadata: Metadata = {
   title: "Create Battle",
 };
 
 export default function CreateBattlesPage() {
-  return <BattleForm />;
+  return (
+    <Suspense fallback={<LoadingAlert />}>
+      <CreateBattleContent />
+    </Suspense>
+  );
 }
