5bbe1578fd70f0bca33600ae865f7a1f97e773d2
imaimai17468 <tosiki_801_life@outlook.jp>
2024-01-03 00:04:29 +0900

    fix: Add mock users to BattleDetailBoard and UserTeamContent

--- src/features/user/team/UserTeamContent/UserTeamContent.tsx
+++ src/features/user/team/UserTeamContent/UserTeamContent.tsx
@@ -13,15 +13,17 @@ import {
 } from "@/components/ui/table";
 import { TeamJoinDialog } from "@/features/battle/detail/BattleDetailBoard/TeamJoinDialog";
 import { createMockTeams } from "@/repositories/createMockTeam";
+import { createMockUsers } from "@/repositories/createMockUser";
 
 export const UserTeamContent = async () => {
   const teams = await createMockTeams(5);
+  const users = await createMockUsers(10);
 
   return (
     <div className="flex flex-col gap-2">
       <div className="flex justify-between">
         <h1 className="text-2xl font-semibold">Your Teams</h1>
-        <TeamJoinDialog>
+        <TeamJoinDialog users={users}>
           <Button className="flex gap-2">
             <PlusCircledIcon />
             <p>Add Team</p>
