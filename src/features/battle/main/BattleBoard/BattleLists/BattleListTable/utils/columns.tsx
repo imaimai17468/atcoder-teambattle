7776b305ee5424013f766b12a310818f9d99a189
imaimai17468 <tosiki_801_life@outlook.jp>
2024-01-06 18:00:22 +0900

    feat: Add useBattleListTable and columns to BattleListTable

---
+++ src/features/battle/main/BattleBoard/BattleLists/BattleListTable/utils/columns.tsx
@@ -0,0 +1,35 @@
+import { createColumnHelper } from "@tanstack/react-table";
+
+import { TimeProgress } from "@/components/common/TimeProgress";
+import { Battle } from "@/schema/Battle.type";
+
+const columnHelper = createColumnHelper<Battle>();
+
+export const columns = [
+  columnHelper.accessor("title", {
+    header: "Title",
+  }),
+  columnHelper.accessor("description", {
+    header: "Description",
+    cell: ({ row }) => <div className="w-96">{row.original.description}</div>,
+  }),
+  columnHelper.display({
+    id: "teams",
+    header: "No of Teams",
+    cell: ({ row }) => (
+      <div>{row.original.scores ? row.original.scores.length : 0}</div>
+    ),
+  }),
+  columnHelper.display({
+    id: "time",
+    header: "Duration",
+    cell: ({ row }) => (
+      <div className="w-64">
+        <TimeProgress
+          startDate={row.original.startDate}
+          endDate={row.original.endDate}
+        />
+      </div>
+    ),
+  }),
+];
