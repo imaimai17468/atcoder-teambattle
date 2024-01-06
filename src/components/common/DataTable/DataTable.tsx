ad34229a9b42c75f933e9c756f096f70674d5595
imaimai17468 <tosiki_801_life@outlook.jp>
2024-01-07 00:05:25 +0900

    feat: Add sorting functionality to DataTable component

--- src/components/common/DataTable/DataTable.tsx
+++ src/components/common/DataTable/DataTable.tsx
@@ -1,9 +1,14 @@
+"use client";
+
 import {
   useReactTable,
   flexRender,
   getCoreRowModel,
   ColumnDef,
+  getSortedRowModel,
+  SortingState,
 } from "@tanstack/react-table";
+import { useState } from "react";
 
 import {
   Table,
@@ -17,17 +22,25 @@ import {
 type DataTableProps<TData> = {
   data: TData[];
   columns: ColumnDef<TData>[];
-  onRowClick: (id: string) => void;
+  onRowClick: (id: TData) => void;
 };
+
 export const DataTable = <TData,>({
   data,
   columns,
   onRowClick,
 }: DataTableProps<TData>) => {
+  const [sorting, setSorting] = useState<SortingState>([]);
+
   const table = useReactTable({
     data,
     columns,
+    state: {
+      sorting,
+    },
+    onSortingChange: setSorting,
     getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
   });
 
   return (
@@ -38,12 +51,25 @@ export const DataTable = <TData,>({
             <TableRow key={headerGroup.id}>
               {headerGroup.headers.map((header) => (
                 <TableHead key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
+                  {header.isPlaceholder ? null : (
+                    <div
+                      {...{
+                        className: header.column.getCanSort()
+                          ? "cursor-pointer select-none"
+                          : "",
+                        onClick: header.column.getToggleSortingHandler(),
+                      }}
+                    >
+                      {flexRender(
                         header.column.columnDef.header,
                         header.getContext(),
                       )}
+                      {{
+                        asc: " ⬆︎",
+                        desc: " ⬇︎",
+                      }[header.column.getIsSorted() as string] ?? null}
+                    </div>
+                  )}
                 </TableHead>
               ))}
             </TableRow>
@@ -54,7 +80,7 @@ export const DataTable = <TData,>({
             <TableRow
               key={row.id}
               className="cursor-pointer hover:bg-gray-800/10"
-              onClick={() => onRowClick(row.id)}
+              onClick={() => onRowClick(row.original)}
             >
               {row.getVisibleCells().map((cell) => (
                 <TableCell key={cell.id}>
