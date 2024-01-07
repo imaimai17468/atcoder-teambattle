8e7ab770aab108d847eb36233532adc422790fb9
imaimai17468 <tosiki_801_life@outlook.jp>
2024-01-08 00:46:37 +0900

    feat: Add pagination and page size options to DataTable component

--- src/components/common/DataTable/DataTable.tsx
+++ src/components/common/DataTable/DataTable.tsx
@@ -1,15 +1,32 @@
 "use client";
 
 import {
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  DoubleArrowLeftIcon,
+  DoubleArrowRightIcon,
+  SlashIcon,
+} from "@radix-ui/react-icons";
+import {
   useReactTable,
   flexRender,
   getCoreRowModel,
-  ColumnDef,
   getSortedRowModel,
+  getPaginationRowModel,
+  ColumnDef,
   SortingState,
 } from "@tanstack/react-table";
 import { useState } from "react";
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import {
   Table,
   TableHead,
@@ -19,17 +36,17 @@ import {
   TableBody,
 } from "@/components/ui/table";
 
-type DataTableProps<TData> = {
-  data: TData[];
-  columns: ColumnDef<TData>[];
-  onRowClick: (id: TData) => void;
+type DataTableProps<T> = {
+  data: T[];
+  columns: ColumnDef<T>[];
+  onRowClick: (id: T) => void;
 };
 
-export const DataTable = <TData,>({
+export const DataTable = <T,>({
   data,
   columns,
   onRowClick,
-}: DataTableProps<TData>) => {
+}: DataTableProps<T>) => {
   const [sorting, setSorting] = useState<SortingState>([]);
 
   const table = useReactTable({
@@ -41,10 +58,11 @@ export const DataTable = <TData,>({
     onSortingChange: setSorting,
     getCoreRowModel: getCoreRowModel(),
     getSortedRowModel: getSortedRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
   });
 
   return (
-    <div className="w-full overflow-x-scroll">
+    <div className="flex w-full flex-col gap-4 overflow-x-scroll">
       <Table>
         <TableHeader>
           {table.getHeaderGroups().map((headerGroup) => (
@@ -91,6 +109,77 @@ export const DataTable = <TData,>({
           ))}
         </TableBody>
       </Table>
+      <div className="flex items-center gap-2">
+        <Button
+          size="icon"
+          variant="outline"
+          onClick={() => table.setPageIndex(0)}
+          disabled={!table.getCanPreviousPage()}
+        >
+          <DoubleArrowLeftIcon />
+        </Button>
+        <Button
+          size="icon"
+          variant="outline"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          <ArrowLeftIcon />
+        </Button>
+        <Button
+          size="icon"
+          variant="outline"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          <ArrowRightIcon />
+        </Button>
+        <Button
+          size="icon"
+          variant="outline"
+          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+          disabled={!table.getCanNextPage()}
+        >
+          <DoubleArrowRightIcon />
+        </Button>
+        <div className="flex items-center gap-2 text-sm">
+          <p>Page</p>
+          <p>{table.getState().pagination.pageIndex + 1}</p>
+          <p>of</p>
+          <p>{table.getPageCount()}</p>
+        </div>
+        <SlashIcon />
+        <div className="flex items-center gap-2 text-sm">
+          <p className="whitespace-nowrap">Go to page:</p>
+          <Input
+            type="number"
+            defaultValue={table.getState().pagination.pageIndex + 1}
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0;
+              table.setPageIndex(page);
+            }}
+          />
+        </div>
+        <Select
+          value={table.getState().pagination.pageSize.toString()}
+          onValueChange={(value) => {
+            table.setPageSize(Number(value));
+          }}
+        >
+          <SelectTrigger className="w-32">
+            <SelectValue>
+              Show {table.getState().pagination.pageSize}
+            </SelectValue>
+          </SelectTrigger>
+          <SelectContent className="w-32">
+            {[10, 25, 50, 75, 100].map((pageSize) => (
+              <SelectItem key={pageSize} value={pageSize.toString()}>
+                Show {pageSize}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </div>
     </div>
   );
 };
