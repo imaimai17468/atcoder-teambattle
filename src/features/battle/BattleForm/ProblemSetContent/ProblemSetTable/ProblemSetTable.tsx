28e0cef6b202b516adeac04c1d691b27dc7c5ce6
imaimai17468 <tosiki_801_life@outlook.jp>
2024-01-08 22:22:57 +0900

    feat: 問題選択テーブルをDataTableに置き換え

--- src/features/battle/BattleForm/ProblemSetContent/ProblemSetTable/ProblemSetTable.tsx
+++ src/features/battle/BattleForm/ProblemSetContent/ProblemSetTable/ProblemSetTable.tsx
@@ -1,15 +1,6 @@
-import { Link1Icon } from "@radix-ui/react-icons";
+import { useColumns } from "./utils/useColumns";
 
-import { DeleteButton } from "@/components/common/DeleteButton";
-import { DifficultMark } from "@/components/common/DifficultMark";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+import { DataTable } from "@/components/common/DataTable";
 import { Problem } from "@/schema/Problem.type";
 
 type ProblemSetTableProps = {
@@ -21,70 +12,6 @@ export const ProblemSetTable: React.FC<ProblemSetTableProps> = ({
   problems,
   setProblems,
 }: ProblemSetTableProps) => {
-  return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-1/6">index</TableHead>
-          <TableHead>Problem</TableHead>
-          <TableHead className="w-1/6 text-center">Difficulty</TableHead>
-          <TableHead className="w-1/6 text-center">Point</TableHead>
-          <TableHead className="w-1/6 text-center">Delete</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {problems.length !== 0 ? (
-          problems?.map((problem, index) => {
-            return (
-              <TableRow
-                key={problem.name}
-                className="cursor-pointer hover:bg-gray-800/10"
-              >
-                <TableCell>{index + 1}</TableCell>
-                <TableCell>
-                  <div className="flex items-center gap-2">
-                    {problem.name}
-                    <a
-                      href="https://www.google.com/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <Link1Icon />
-                    </a>
-                  </div>
-                </TableCell>
-                <TableCell className="text-center">
-                  <div className="flex items-center justify-center gap-4">
-                    <DifficultMark difficulty={problem.difficulty} />
-                    <p>{problem.difficulty.toFixed(1)}</p>
-                  </div>
-                </TableCell>
-                <TableCell className="text-center">{problem.score}</TableCell>
-                <TableCell className="text-center">
-                  <DeleteButton
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      setProblems((prev) => {
-                        if (!prev) return [];
-                        return prev.filter(
-                          (selectedProblem) =>
-                            selectedProblem.name !== problem.name,
-                        );
-                      });
-                    }}
-                  />
-                </TableCell>
-              </TableRow>
-            );
-          })
-        ) : (
-          <TableRow>
-            <TableCell className="text-center" colSpan={5}>
-              No Data
-            </TableCell>
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
-  );
+  const columns = useColumns({ setProblems });
+  return <DataTable<Problem> data={problems} columns={columns} draggable />;
 };
